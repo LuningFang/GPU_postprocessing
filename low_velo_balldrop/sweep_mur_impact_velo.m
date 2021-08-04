9e049488dc6884c998b8ada5aacd897814f5222b
LuningFang <lfang9@wisc.edu>
2021-08-04 09:53:00 -0500

    use Cecily's code to post process results, add low-pass filter to acceleration

--- low_velo_balldrop/sweep_mur_impact_velo.m
+++ low_velo_balldrop/sweep_mur_impact_velo.m
@@ -1,83 +1,95 @@
-%close all
-% clc
-%clear all
+close all
+clc
+clear all
 
 % given
 figure(1)
 addpath('../plot_tools/');
 addpath('helpers/');
+impact_velo_array=0.3:0.05:1;
+mur_array = [0 0.05, 0.09, 0.2];
+plot_style_array = {'g*', 'rd', 'b+', 'ms'};
 
 gravity = 9.8;
-%threshold_acc = 0.01; % threshold for determine collision time
-threshold_acc = 0.1 * gravity; % threshold for determine collision time
+simulation_time_step = 1e-4;
+unit_scale = 1;
+numTests = length(impact_velo_array);
+depth_array = zeros(numTests, 1);
+collision_time_array = zeros(numTests, 1);
+peak_acc_array = zeros(numTests, 1);
+actual_collision_velo_array = zeros(numTests, 1);
 
 spacing = 1;
 
-mur=0.2;
-directory=sprintf('data/sweep_tests/mur_%.2f/', mur);
-impact_velo_array=0.25:0.05:1;
+for jj = 1:length(mur_array)
+    
+    mur = mur_array(jj);
+    plot_style = plot_style_array{jj};
+    directory = sprintf('data/sweep_tests/mur_%.2f/', mur);
+    
+    for ii = 1:length(impact_velo_array)
+        impact_velo = impact_velo_array(ii);
+        filename = strcat(directory, sprintf('bc_sphere_result_mur_%.2f_velo_%.2fms-1.csv', mur, impact_velo));
+        filename
+        result = csvread(filename);
+        fdat.time = result(1:spacing:end,1);
+        fdat.pos_m = result(1:spacing:end,2) *0.01; % input data from chrono in cm
+        fdat.vel_m = result(1:spacing:end,3);
+        fdat.acc_m = result(1:spacing:end,4);
+        
+        pdat = calc_collision(fdat, simulation_time_step, gravity, unit_scale);
+        
+        actual_col_velo = abs(pdat.vcollision);
+        actual_collision_velo_array(ii) = actual_col_velo;
+        depth_array(ii)                 = pdat.zstop3 - pdat.zinit;
+        collision_time_array(ii)        = pdat.tstop3 - pdat.tstart;
+        peak_acc_array(ii)              = pdat.apeak;
+        
+    end
+    
+    LW = 2;
+    FS = 25;
+    MS = 10;
+    
+    subplot(1,3,1)
+    hold on
+    hdl = makePlot(actual_collision_velo_array, peak_acc_array, 'impact velocity (m/s)', 'peak acceleration (ms^{-2})', '', LW, FS, plot_style, MS);
+    xlim([0, 1.25]);
+    ylim([0, 90]);
+    ax = hdl.Parent;
+    set(ax, 'XTick', 0:0.25:1.25)
+    set(ax, 'YTick', 0:18:90)
+    grid on
+    if (jj == 4)
+        hdl.Color=[1 0.5 0];
+    end
+    
+    subplot(1,3,2)
+    hold on
+    hdl = makePlot(actual_collision_velo_array, depth_array*100, 'impact velocity (m/s)', 'penetration depth(cm)', '', LW, FS, plot_style, MS);
+    xlim([0, 1.25]);
+    ylim([0, 6]);
+    ax = hdl.Parent;
+    set(ax, 'XTick', 0:0.25:1.25)
+    set(ax, 'YTick', 0:1.2:6)
+    grid on
+    if (jj == 4)
+        hdl.Color=[1 0.5 0];
+    end
+    
+    
+    subplot(1,3,3)
+    hold on
+    hdl = makePlot(actual_collision_velo_array, collision_time_array * 1000, 'impact velocity (m/s)', 'collision time(ms)', '', LW, FS, plot_style, MS);
+    xlim([0, 1.25]);
+    ylim([0, 200]);
+    ax = hdl.Parent;
+    set(ax, 'XTick', 0:0.25:1.25)
+    set(ax, 'YTick', 0:40:200)
+    grid on
+    if (jj == 4)
+        hdl.Color=[1 0.5 0];
+    end
 
-depth_array = zeros(length(impact_velo_array), 1);
-collision_time_array = zeros(length(impact_velo_array), 1);
-peak_acc_array = zeros(length(impact_velo_array), 1);
-
-for ii = 1:length(impact_velo_array)
-    impact_velo = impact_velo_array(ii);
-    filename = strcat(directory, sprintf('bc_sphere_result_mur_%.2f_velo_%.2fms-1.csv', mur, impact_velo));
-    filename
-    result = csvread(filename);
-    time = result(1:spacing:end,1);
-    position = result(1:spacing:end,2);
-    velo = result(1:spacing:end,3);
-    acc = result(1:spacing:end,4);
-    
-    [t_contact_start, index_start] = findCollisionStartTime(acc, time);
-    
-    initial_position = position(index_start);
-    
-    % throw away useless data
-    time = time(index_start:end);
-    position = position(index_start:end);
-    velo = velo(index_start:end);
-    acc = acc(index_start:end);
-    
-    [t_contact_end, index_end] = findCollisionEndTime(acc, time, threshold_acc);
-    
-    depth = position(index_end) - initial_position;
-    depth_array(ii) = depth;
-    collision_time = t_contact_end - t_contact_start;
-    collision_time_array(ii) = collision_time;
-    
-    peak_acc_array(ii) = max(acc);
 end
-
-LW = 2;
-FS = 25;
-MS = 10;
-
-subplot(1,3,1)
-hold on
-hdl = makePlot(impact_velo_array, peak_acc_array, 'impact velocity (m/s)', 'peak acceleration (ms^{-2})', '', LW, FS, 'o', MS);
-xlim([0, 1.25]);
-%ylim([0, 200]);
-ax = hdl.Parent;
-set(ax, 'XTick', 0:0.25:1.25)
-grid on
-
-subplot(1,3,2)
-hold on
-hdl = makePlot(impact_velo_array, -depth_array, 'impact velocity (m/s)', 'penetration depth(cm)', '', LW, FS, 'o', MS);
-xlim([0, 1.25]);
-%ylim([0, 8]);
-ax = hdl.Parent;
-set(ax, 'XTick', 0:0.25:1.25)
-grid on
-
-subplot(1,3,3)
-hold on
-hdl = makePlot(impact_velo_array, collision_time_array * 1000, 'impact velocity (m/s)', 'collision time(ms)', '', LW, FS, 'o', MS);
-xlim([0, 1.25]);
-%ylim([0, 200]);
-ax = hdl.Parent;
-set(ax, 'XTick', 0:0.25:1.25)
-grid on
+legend('\mu_r = 0', '\mu_r = 0.05', '\mu_r = 0.09', '\mu_r = 0.2');
